d8853a520c7ea500a9c104ec2b78567f2683b841
Linus Hellman <linkan360@gmail.com>
2016-12-01 17:01:26 +0100

    Working (barely) PI regulator

--- GG.c
+++ GG.c
@@ -3,28 +3,29 @@
 #define PIN_INT1	(1 << PB1)
 #define PIN_INT2	(1 << PB2)
 #define POWER_PWM	(1 << PD3)
-#define CONV_RPM 7268/2
+#define CONV_RPM 39063
 #define CLK_FREQ 1000000
 #define READ_PERIOD 1
 #define CTRL_PERIOD 90
+#define MEAN 8
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 static volatile int speed;
 static volatile int tmp;
-static volatile int times[8];
-static volatile char arrayPos;
+static volatile int times[MEAN];
+static volatile unsigned char arrayPos;
 
 //Controll variables
 
-volatile unsigned char u = 39;
-volatile unsigned int ref = 90;
+volatile unsigned char u = 0;
+volatile unsigned int ref = 40;
 
-volatile double K = 1;
+volatile double K = 1.4;
 volatile double I = 0;
-volatile double Ti = 1;
-volatile double Tr = 1;
+volatile double Ti = 0.6;
+volatile double Tr = 0.6;
 volatile double h = 1.0 / (CLK_FREQ / 1024 / CTRL_PERIOD); 
 
 unsigned char saturate(signed int in)
@@ -33,7 +34,7 @@ unsigned char saturate(signed int in)
 		return 255;
 	if(in < 0)
 		return 0;
-	return (in & 0xff);
+	return in;
 }
 
 void io_init(void)
@@ -53,12 +54,12 @@ void timer_init(void)
 {
 	//Timer 1 init
 	TCCR1B	|= (1 << CS01)|(1 << CS00);
-	TCNT1 = 0x0000;
+	TIMSK1 |= (1 << TOIE1);
 
 	//Timer 0 init
 	TCCR0A |= (1 << WGM01);
 	TCCR0B |= (1 << CS02) | (1 << CS00);
-	//TIMSK0 |= (1 << OCIE0A);
+	TIMSK0 |= (1 << OCIE0A);
 	OCR0A = CTRL_PERIOD;
 
 }
@@ -73,6 +74,11 @@ void pwm_init(void)
 
 }
 
+void set_pwm(char input)
+{
+	OCR2B = (input ^ 0xff);
+}
+
 int main(void)
 {
 	io_init();
@@ -84,14 +90,14 @@ int main(void)
     while(1)
     {
 
-		if(speed > 315)
+    	/*if(speed > 100 && speed < 110)
 		{
 			PORTB |= LED_LEFT;
 		}
 		else
 		{
-			PORTB & ~LED_LEFT;
-		}
+			PORTB &= ~LED_LEFT;
+		}*/
 
     }
 }
@@ -100,36 +106,49 @@ ISR(PCINT0_vect)
 {
 	//PORTB ^= LED_LEFT;
 
-	times[arrayPos & 7] = TCNT1;
-	TCNT1 = 0;
-
-	if((arrayPos & READ_PERIOD) == 1)
+	if((PINB & 4) > 0)
 	{
-		//PORTB ^= LED_LEFT;
 
+		//PORTD ^= LED_RIGHT;
 
-		tmp = 0;
+		times[arrayPos & (MEAN - 1)] = TCNT1;
+		TCNT1 = 0;
 
-		for(int x = 0; x < 8; x = x + 1)
+		if((arrayPos & READ_PERIOD) == 1)
 		{
-			tmp = tmp + times[x];
+			//PORTB ^= LED_LEFT;
+
+
+			tmp = 0;
+
+			for(int x = 0; x < MEAN; x = x + 1)
+			{
+				tmp = tmp + times[x];
+			}
+
+			tmp = tmp >> 3;
+			speed = 1.0 * CONV_RPM / tmp - 2;
+
 		}
 
-		//tmp = tmp >> 3;
-		speed = CONV_RPM / tmp;
-
+		arrayPos = arrayPos + 1;
 	}
-
-	arrayPos = arrayPos + 1;
 }
 
-/*ISR(TIMER0_COMPA_vect)
+ISR(TIMER0_COMPA_vect)
 {
+	//PORTB ^= LED_LEFT;
+
 	signed int error = ref - speed;
 	signed int v = I + K * error;
 	u = saturate(v);
-	//set duty with u
 
-	//Calculate integral for next round
+	set_pwm(u);
+	
 	I = I + (K * h / Ti) * error + (h / Tr) * (u - v);
-}*/
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	speed = 0;
+}
